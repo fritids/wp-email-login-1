9b3960fe0c1cd806622e3731277602dc7848975f
beaulebens <beaulebens@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-30 23:28:39 +0000

    Add translatable strings, include Dutch one props andykillen. Adding .po file as well.
    git-svn-id: https://plugins.svn.wordpress.org/wp-email-login/trunk@338973 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- email-login.php
+++ email-login.php
@@ -3,16 +3,19 @@
 Plugin Name: Email Login
 Plugin URI: http://dentedreality.com.au/projects/wp-plugin-email-login/
 Description: Allows you to log into WordPress (directly or via XML-RPC) using your email address instead of a(nother) username.
-Author: Beau Lebens, r-a-y
+Author: Beau Lebens
 Version: 4.0
 Author URI: http://dentedreality.com.au/
 */
 
 /**
- * Replace default filter for authentication with a new one that looks up a username first if an email is passed in
+ * Load language files for translating strings. Props andykillen.
  */
-remove_filter( 'authenticate', 'wp_authenticate_username_password', 20, 3 );
-add_filter( 'authenticate', 'dr_email_login_authenticate', 20, 3 );
+function dr_email_load_language() {
+	$plugin_dir = basename( dirname( __FILE__ ) );
+	load_plugin_textdomain( 'email-login', null, $plugin_dir );
+}
+add_action( 'init', 'dr_email_load_language', 1 );
 
 /**
  * If an email address is entered in the username box, then look up the matching username and authenticate as per normal, using that.
@@ -29,41 +32,43 @@ function dr_email_login_authenticate( $user, $username, $password ) {
 	
 	return wp_authenticate_username_password( null, $username, $password );
 }
+remove_filter( 'authenticate', 'wp_authenticate_username_password', 20, 3 );
+add_filter( 'authenticate', 'dr_email_login_authenticate', 20, 3 );
 
 /**
  * Add compatibility for WPMU 2.9.1 and WPMU 2.9.2, props r-a-y
  */
 if ( !function_exists( 'is_super_admin' ) ) :
 	function get_super_admins() {
-	    global $super_admins;
-
-	    if ( isset( $super_admins ) )
-	        return $super_admins;
-	    else
-	        return get_site_option( 'site_admins', array( 'admin' ) );
+		global $super_admins;
+		
+		if ( isset( $super_admins ) )
+			return $super_admins;
+		else
+			return get_site_option( 'site_admins', array( 'admin' ) );
 	}
-
+	
 	function is_super_admin( $user_id = false ) {
-	    if ( ! $user_id ) {
-	        $current_user = wp_get_current_user();
-	        $user_id = ! empty( $current_user ) ? $current_user->id : 0;
-	    }
-
-	    if ( ! $user_id )
-	        return false;
-
-	    $user = new WP_User( $user_id );
-
-	    if ( is_multisite() ) {
-	        $super_admins = get_super_admins();
-	        if ( is_array( $super_admins ) && in_array( $user->user_login, $super_admins ) )
-	            return true;
-	    } else {
-	        if ( $user->has_cap( 'delete_users' ) )
-	            return true;
-	    }
-
-	    return false;
+		if ( ! $user_id ) {
+			$current_user = wp_get_current_user();
+			$user_id = ! empty( $current_user ) ? $current_user->id : 0;
+		}
+		
+		if ( ! $user_id )
+			return false;
+		
+		$user = new WP_User( $user_id );
+		
+		if ( is_multisite() ) {
+			$super_admins = get_super_admins();
+			if ( is_array( $super_admins ) && in_array( $user->user_login, $super_admins ) )
+				return true;
+		} else {
+			if ( $user->has_cap( 'delete_users' ) )
+				return true;
+		}
+		
+		return false;
 	}
 endif;
 
@@ -73,11 +78,11 @@ endif;
 function username_or_email_login() {
 	?><script type="text/javascript">
 	// Form Label
-	document.getElementById('loginform').childNodes[1].childNodes[1].childNodes[0].nodeValue = 'Username or Email';
+	document.getElementById('loginform').childNodes[1].childNodes[1].childNodes[0].nodeValue = '<?php _e( 'Username or Email', 'email-login' ); ?>';
 	
 	// Error Messages
 	if ( document.getElementById('login_error') )
-		document.getElementById('login_error').innerHTML = document.getElementById('login_error').innerHTML.replace( 'username', 'username or email' );
+		document.getElementById('login_error').innerHTML = document.getElementById('login_error').innerHTML.replace( '<?php _e( 'username', 'email-login' ); ?>', '<?php _e( 'Username or Email', 'email-login' ); ?>' );
 	</script><?php
-}
+} 
 add_action( 'login_form', 'username_or_email_login' );
--- readme.txt
+++ readme.txt
@@ -1,17 +1,20 @@
 === WP Email Login ===
-Contributors: beaulebens, r-a-y
+Contributors: beaulebens
 Tags: email, login, authentication, users, admin
 Requires at least: 2.8
 Tested up to: 3.1-alpha
 Stable tag: trunk
-Donate link: http://dentedreality.com.au
 
 Lets you use your email address to log into your WordPress account instead of a username.
 
 == Description ==
 Lets you use your email address to log into your WordPress account instead of a username.
 
-Since email addresses are required to be unique within WordPress anyway, they also make good identifiers for logging in.
+Since email addresses are required to be unique within WordPress anyway, they also make good identifiers for logging in. For slightly better security, set your username to something random and then just forget it and use your email address instead.
+
+Special thanks to:
+* r-a-y for compatibility with older versions of WPMU and XML-RPC
+* andykillen for introducing translatable strings and the Dutch translation
 
 == Installation ==
 1. Unzip and upload `/wp-email-login/` to the `/wp-content/plugins/` directory
@@ -19,6 +22,10 @@ Since email addresses are required to be unique within WordPress anyway, they al
 1. Log out, log in again using the email address associated with your WordPress account.
 
 == Changelog ==
+= 4.1 =
+* Clean up code a bit
+* Introduce translatable strings, props andykillen. Packaged with Dutch translation.
+
 = 4.0 =
 * Add prompt to login form that you can use Email as well
 * Use get_user_by_email(), props Hendry (via email)
